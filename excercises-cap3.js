10a1ba90b05640aeee35eea25581f94ee40e2db1
juliosam <ja.samaniegom@gmail.com>
2022-03-17 11:49:13 -0500

    excercises 1 and 2

--- excercises-cap3.js
+++ excercises-cap3.js
@@ -2,3 +2,37 @@ let ex1 = "Ejercicio 1";
 
 console.log(ex1);
 
+let space =" \n  \n  ";
+
+let min = function(argumento1,argumento2){
+    if(argumento1>argumento2){
+        return argumento2
+    }
+    else{return argumento1}
+}
+
+console.log(min(0,10));
+console.log(min(0,-10));
+
+console.log(space);
+
+let ex2 = "Ejercicio 2";
+
+console.log(ex2);
+
+let isEven = function(isIt){
+    if(isIt<0){return "??"}
+    else if(isIt%2 == 0){return true}
+    else { return false}
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(0));
+
+console.log(space);
+
+let ex3 = "Ejercicio 3";
+
+console.log(ex3);
